418eaf9eabc857419b578088e07160a525c8cf75
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-08-30 22:22:27 +0900

    LinkedList search

--- LinkedList.js
+++ LinkedList.js
@@ -1,5 +1,6 @@
 class LinkedList {
   length = 0;
+  // 처음에 있는 것
   head = null;
 
   add(value) {
@@ -12,17 +13,27 @@ class LinkedList {
     } else {
       this.head = new Node(value);
     }
-    this.length++
+    this.length++;
     return this.length;
   }
 
-  search(value) {}
+  // index가 몇번 넘기는지를 의미
+  search(index) {
+    let count = 0;
+    let current = this.head;
+    while (count < index) {
+      current = current?.next;
+      count++;
+    }
+    return current?.value;
+  }
 
   remove(value) {}
 }
 
 class Node {
   next = null;
+  // 외부에서 전달받을 값은 constructor 써줘야함.
   constructor(value) {
     this.value = value;
   }
@@ -35,8 +46,11 @@ ll.add(2);
 ll.add(3);
 ll.add(4);
 ll.add(5);
-console.log(ll.add(6));
-// ll.search(4);
+ll.add(6);
+console.log(ll.search(3)); // 4
+console.log(ll.search(5)); // 6
+console.log(ll.search(7)); // undefined
+
 // ll.search(7); // null
 // ll.remove(4);
 // ll.search(4); // null
