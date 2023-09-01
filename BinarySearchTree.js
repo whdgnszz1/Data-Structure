a9086bbc8c9107a848353dd84e395e9e553ca3ca
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-01 21:08:03 +0900

    BinarySearchTree remove

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -61,9 +61,58 @@ class BinarySearchTree {
     this.#search(this.root, value);
   }
 
+  #remove(node, value) {
+    if (!node) {
+      // 제거할 값이 bst에 존재하지 않는 경우
+      return false; // 지울 값이 존재하지 않으면 false return
+    }
+    if (node.value === value) {
+      // 자식입장
+      // 지울 값을 찾은 경우
+      if (!node.left && !node.right) {
+        // leaf
+        return null;
+      } else if (!node.left) {
+        // 왼팔만 없는 경우
+        return node.right;
+      } else if (!node.right) {
+        // 오른팔만 없는 경우
+        return node.left;
+      } else {
+        // 양팔 다 없는 경우
+        const exchange = node.left;
+        while (exchange.right) {
+          exchange = exchange.right;
+        }
+        const temp = node.value;
+        node.value = exchange.value;
+        exchange.value = temp;
+        node.left = this.#remove(node.left, temp);
+        return node;
+      }
+    } else {
+      // 부모입장
+      if (node.value > value) {
+        // return null이 들어오므로 node.left가 null이 된다.
+        // 재귀에선 자식이 return하는 값이 부모로 전달됨.
+        node.left = this.#remove(node.left, value);
+        return node;
+      } else {
+        node.right = this.remove(node.right, value);
+        return node;
+      }
+    }
+  }
 
-  // 양팔인애: 자기 왼쪽중에서 가장 오른쪽
-  remove(value) {}
+  // 1. 양팔이 다 없는 경우: 부모한테 자기자신을 잘라달라고 한다.
+  // 2. 왼팔만 있거나 오른팔만 있는 경우: 부모한테 자신을 자식으로 대체해달라고 한다.
+  // 3. 양팔인 경우: 자기 왼쪽중에서 가장 오른쪽에 있는애와 자리를 바꾸고 제거한다.
+  remove(value) {
+    const node = this.#remove(this.root, value);
+    if (node) {
+      this.root = node;
+    }
+  }
 }
 
 class Node {
